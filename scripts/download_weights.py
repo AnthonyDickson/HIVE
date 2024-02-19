e1aaa57ffea076a5743beff890eeb5921c2fafbc
Anthony Dickson <dican732@student.otago.ac.nz>
2024-02-19 18:25:09 +1300

    Add missing LPIPS package to requirements.txt

--- scripts/download_weights.py
+++ scripts/download_weights.py
@@ -14,61 +14,10 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import argparse
-import os
-from zipfile import ZipFile
-
-import torch
-import wget
 from detectron2.config import get_cfg
 from detectron2.engine import DefaultPredictor
 from detectron2.model_zoo import model_zoo
-
-
-def get_model_from_url(
-    url: str, local_path: str, is_zip: bool = False, path_root: str = "checkpoints"
-) -> str:
-    local_path = os.path.join(path_root, local_path)
-    if os.path.exists(local_path):
-        print(f"Found cache {local_path}")
-        return local_path
-
-    # download
-    local_path = local_path.rstrip(os.sep)
-    download_path = local_path if not is_zip else f"{local_path}.zip"
-    os.makedirs(os.path.dirname(download_path), exist_ok=True)
-    if os.path.isfile(download_path):
-        print(f"Found cache {download_path}")
-    else:
-        print(f"Dowloading {url} to {download_path} ...")
-        wget.download(url, download_path)
-
-    if is_zip:
-        print(f"Unziping {download_path} to {local_path}")
-        with ZipFile(download_path, 'r') as f:
-            f.extractall(local_path)
-        os.remove(download_path)
-
-    return local_path
-
-
-
-def get_adabins_weights(basemodel_name='tf_efficientnet_b5_ap'):
-    print('Loading base model {}...'.format(basemodel_name), end='')
-    torch.hub.load('rwightman/gen-efficientnet-pytorch', basemodel_name, pretrained=True)
-
-    print("Finished downloading EfficientNet weights for AdaBins.")
-
-
-def get_mc_weights():
-    get_model_from_url(
-        "https://storage.googleapis.com/mannequinchallenge-data/checkpoints/best_depth_Ours_Bilinear_inc_3_net_G.pth",
-        local_path="mc.pth",
-        path_root=os.environ["WEIGHTS_PATH"]
-    )
-
-    print("Finished downloading Mannequin Challange (mc) weights.")
-
+import lpips
 
 def get_detectron2_weights():
     cfg = get_cfg()
@@ -83,19 +32,10 @@ def get_detectron2_weights():
 
     print("Finished downloading Detectron2 weights.")
 
+def download_lpips_weights():
+    _ = lpips.LPIPS(net='alex')
+
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('--mc', help='Download the weights for the "Mannequin Challenge" depth estimation model.', action='store_true')
-    parser.add_argument('--adabins', help='Download the weights for the AdaBins depth estimation model.', action='store_true')
-
-    args = parser.parse_args()
-
     get_detectron2_weights()
-
-    if args.mc:
-        get_mc_weights()
-
-    if args.adabins:
-        get_adabins_weights()
+    download_lpips_weights()
